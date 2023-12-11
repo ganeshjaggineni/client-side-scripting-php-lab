7bae071613045672ef1dfa65f2e486db0d8e9198
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2023-12-11 15:52:13 +0530

    case sensitivity in php

---
+++ case_senitivity_echo_ECHO.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+	<h1>CASE SENSITIVITY</h1>
+	<?php
+	echo "ganesh jaggineni</br>";
+	ECHO "ganesh jaggieneni</br>";
+	eCHO "gani</br>";
+	echO "gana</br>";
+	eCHo "ganesh</br>";
+	?>
+</body>
+</html>
