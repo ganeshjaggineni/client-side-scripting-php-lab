7277a6d99c224bc27b785ba68067e4d84045d1d7
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2023-12-11 15:52:34 +0530

    case sensitive variables in php

---
+++ case_sensitivity_color_COLOR.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <h1>CASE SENSITIVITY</h1>
+        <?php
+        $color = "blue";
+        echo "my house is in " . $color . "<br>";
+        echo "my watch is in " . $Color . "<br>";
+        ?>
+    </body>
+</html>
