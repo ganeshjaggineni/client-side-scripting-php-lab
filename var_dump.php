014ee8899ffc56d62e87b4975cbd23d4969d0adb
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2023-12-11 15:53:21 +0530

    var dump method in php

---
+++ var_dump.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+<?php
+var_dump(5);
+echo "<br>";
+var_dump("ganesh");
+echo "<br>";
+var_dump(true);
+echo "<br>";
+var_dump(2.33);
+echo "<br>";
+var_dump([10,23,34]);
+?>
+</body>
+</html>
